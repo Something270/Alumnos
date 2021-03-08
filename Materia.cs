1f72cc7241cf4197672dad773f311baf5aa98961
Something270 <55809521+Something270@users.noreply.github.com>
2021-03-08 11:02:17 -0700

    Added materia y semestre

---
+++ Materia.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Alumnos
+{
+    class Materia
+    {
+        public string nombre;
+        public int semestre;
+
+
+        public Materia(string nombre, int semestre){
+            this.nombregit = nombre;
+            this.semestre = semestre;
+        }
+    }
+}
