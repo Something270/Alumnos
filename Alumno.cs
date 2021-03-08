67b448806482118e924270353a95958be47cfb1b
Something270 <55809521+Something270@users.noreply.github.com>
2021-03-08 10:18:20 -0700

    Added alumnos y matricula

---
+++ Alumno.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Alumnos
+{
+    class Alumno : Persona
+    {
+        public int matricula
+    }
+
+     public Alumno(string nombre, string apellido, int matricula)
+     {
+         this.nombre= nombre;
+         this.apellido=apellido;
+         this.matricula=matricula;
+     }
+}
